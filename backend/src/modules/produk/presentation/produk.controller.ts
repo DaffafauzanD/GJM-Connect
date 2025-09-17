fa97a8a89faf8b94ebeac35c875de542427f741c
DaffafauzanD <daffafauzand24@gmail.com>
2025-09-17 11:02:32 +0700

    style(produk): format imports and improve code readability in ProdukController

--- backend/src/modules/produk/presentation/produk.controller.ts
+++ backend/src/modules/produk/presentation/produk.controller.ts
@@ -10,7 +10,15 @@ import {
   Delete,
 } from '@nestjs/common';
 import { ProdukService } from '../application/services/produk.service';
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiBadRequestResponse, ApiUnauthorizedResponse, ApiConflictResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+  ApiBadRequestResponse,
+  ApiUnauthorizedResponse,
+  ApiConflictResponse,
+} from '@nestjs/swagger';
 import { ProdukDto } from 'src/modules/produk/application/dto/produk.dto';
 import { PaginationDto } from 'src/common/dto';
 import { CreateProdukDto } from '../application/dto/create-produk.dto';
@@ -20,40 +28,40 @@ import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 @Controller('Products')
 export class ProdukController {
   constructor(private readonly produkService: ProdukService) {}
-    @UseGuards(JwtAuthGuard)
-    @Get()
-    async findAll(@Query() pagination: PaginationDto) {
-        return this.produkService.findAllProduks(pagination);
-    }
-    @UseGuards(JwtAuthGuard)
-    @Get(':id')
-    async findById(@Param('id') id: string) {
-        return this.produkService.findProdukById(id);
-    }
-    @UseGuards(JwtAuthGuard)
-    @Post()
-    @ApiConflictResponse({
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async findAll(@Query() pagination: PaginationDto) {
+    return this.produkService.findAllProduks(pagination);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.produkService.findProdukById(id);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  @ApiConflictResponse({
     description: 'Product with this name already exists or validation error',
     schema: {
       example: {
         success: false,
         message: 'Product with this name already exists',
-        errors: ["Duplicate product name"],
+        errors: ['Duplicate product name'],
         timestamp: '2025-08-25T10:27:34.302Z',
       },
     },
-})
-    async create(@Body() produkDto: CreateProdukDto) {
-        return this.produkService.createProduk(produkDto);
-    }
-    @UseGuards(JwtAuthGuard)
-    @Put(':id')
-    async update(@Param('id') id: string, @Body() produkDto: Partial<ProdukDto>) {
-        return this.produkService.updateProduk(id, produkDto);
-    }
-    @UseGuards(JwtAuthGuard)
-    @Delete(':id')
-    async delete(@Param('id') id: string) {
-        return this.produkService.deleteProduk(id);
-    }
+  })
+  async create(@Body() produkDto: CreateProdukDto) {
+    return this.produkService.createProduk(produkDto);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() produkDto: Partial<ProdukDto>) {
+    return this.produkService.updateProduk(id, produkDto);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.produkService.deleteProduk(id);
+  }
 }
